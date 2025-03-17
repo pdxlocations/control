6d882395905843ea763b85c57df5d93d6a104f5c
pdxlocations <benlipsey@gmail.com>
2025-03-17 16:58:44 -0700

    https

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 '''
-Control -  by http://github.com/pdxlocations
+Control -  by https://github.com/pdxlocations
 Powered by Meshtastic.org
 Meshtastic® is a registered trademark of Meshtastic LLC. Meshtastic software components are released under various licenses, see GitHub for details. No warranty is provided - use at your own risk.
 '''
